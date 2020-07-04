53f6af063bc54cea7673fa6c3be76a739b95b68a
arun <arunmodgil@gmail.com>
2020-07-04 12:11:36 +0100

    Added some logs

--- src/test/java/com/test/demo/DemoApplicationTests.java
+++ src/test/java/com/test/demo/DemoApplicationTests.java
@@ -1,27 +1,31 @@
 package com.test.demo;
 
 
+import com.test.demo.service.TestPetService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.test.demo.DTO.PetResponseDTO;
-import com.test.demo.service.TestWireService;
+import com.test.demo.service.TestPetService;
 
 @SpringBootTest
 class DemoApplicationTests {
 		
 	   @Autowired
-	    private TestWireService service;
+	    private TestPetService service;
 
 		@Test	
 		public void checkCount() {
-			
-			 PetResponseDTO response = this.service.getMyPetCountByStatus("available","doggie");
-			 
-			 if(response.petCount<0) {
+
+			PetResponseDTO response = this.service.getMyPetCountByStatus("available","doggie");
+
+			System.out.println("hello here is the count  => "+response.petCount);
+
+			if(response.petCount<0) {
 				 Assert.fail(response.errorMessage);
+				 System.out.println("hello here is the count --> "+response.petCount);
 			 }
 		}
 }
